4f5c4c8721262f44fa362f981e726ea0a0baaa6b
zulfiyebiser <54160287+zulfiyebiser@users.noreply.github.com>
2019-11-30 12:47:56 -0600

    Update practice.java
    added  String name

--- src/test/java/test/practice.java
+++ src/test/java/test/practice.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class practice {
     private int a;
+    public String name;
     public static void main(String[] args) {
        WebDriverManager.chromedriver().setup();
         ChromeDriver driver = new ChromeDriver();

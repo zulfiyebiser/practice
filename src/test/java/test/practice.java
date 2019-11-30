d1b924d1c092037d9389729c379311bce551ddb9
zulfiyebiser <zlfdnbsr@gmail.com>
2019-11-30 12:38:17 -0600

    added a instance variable

--- src/test/java/test/practice.java
+++ src/test/java/test/practice.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class practice {
+    private int a;
     public static void main(String[] args) {
        WebDriverManager.chromedriver().setup();
         ChromeDriver driver = new ChromeDriver();
